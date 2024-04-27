0704ede008a66a3a3cf9e78848e3db3106e7db30
yenslife <77geo5rge6@gmail.com>
2024-04-27 11:34:32 +0800

    Update json write
    更新了 json 在讀取以及寫入時的問題

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from generate_QA.generate_QA import generate_QA
 from modelTool.mediaKit import dir_to_text_and_srt_files
 from modelTool.summarize import dir_long_text_to_summary_files
 from modelTool.yt import yt_playlist_url_to_mp4
+from rich import print
+from rich.traceback import install
+
 import os
 
+install()
 
 def main():
 
@@ -22,47 +26,48 @@ def main():
     playlist_url = "https://youtube.com/playlist?list=PLm778hWdXOZmH0PF2PIE-M7HfzjZZokdR&si=WuG3_n8Jrj-Jk_TX"
     plain_text_dir_path = f"/home/brick2/platform2024/src/{course_name}/plain_text"
     srt_dir_path = f"/home/brick2/platform2024/src/{course_name}/srt"
-    summary_dir_path = f"/home/brick2/platform2024/src/{course_name}/summay"
+    summary_dir_path = f"/home/brick2/platform2024/src/{course_name}/summary"
     video_dir_path = f"/home/brick2/platform2024/src/{course_name}/video"
     output_dataset_json_path = f"/home/brick2/platform2024/src/{course_name}/dataset.json"
     base_model = "yentinglin/Taiwan-LLM-7B-v2.0-base"
     llm_adapter_path = f"/home/brick2/platform2024/src/{course_name}/llm_adapter"
     finetune_script_path = f"/home/brick2/platform2024/LLM-automation/alpaca-lora/finetune.py"
     
-    # 1. 下載影片
-    print("下載影片")
-    yt_playlist_url_to_mp4(playlist_url, video_dir_path)
+    # # 1. 下載影片
+    # print("下載影片")
+    # yt_playlist_url_to_mp4(playlist_url, video_dir_path)
 
-    # 2. 生成文字檔和 srt 檔案
-    print("生成文字檔和 srt 檔案")
-    dir_to_text_and_srt_files(video_dir_path, plain_text_dir_path, srt_dir_path, model_size="large")
+    # # 2. 生成文字檔和 srt 檔案
+    # print("生成文字檔和 srt 檔案")
+    # dir_to_text_and_srt_files(video_dir_path, plain_text_dir_path, srt_dir_path, model_size="large")
 
-    # 3. 生成摘要檔案 (長文本摘要演算法)
-    print("生成摘要檔案")
-    dir_long_text_to_summary_files(plain_text_dir_path, summary_dir_path)
+    # # 3. 生成摘要檔案 (長文本摘要演算法)
+    # print("生成摘要檔案")
+    # dir_long_text_to_summary_files(plain_text_dir_path, summary_dir_path)
 
     # 4. 生成 QA 檔案
-    run_times = 3
+    run_times = 30
     print(f"生成 QA 檔案，{run_times}次迭代")
     import json
-    # 嘗試讀取 output_dataset_json_path 到 json_obj，如果失敗，則初始化為空 list
     json_obj = []
-    try:
-        json_obj = json.load(open(output_dataset_json_path, "r"))
-    except:
-        json_obj = []
-    for _ in range(run_times):
-        generate_QA(plain_text_dir_path, output_dataset_json_path, summary_path=summary_dir_path)
-        json_obj = json.load(open(output_dataset_json_path, "r")) + json_obj
+    for i in range(run_times):
+        print(f"第 {i+1} 次迭代")
+        generate_QA(plain_text_dir_path, output_dataset_json_path, summary_path=summary_dir_path) # 會自動將資料加到路徑中，所以不用再加一次
+        try:
+            json_obj = json.load(open(output_dataset_json_path, "r"))
+            print(f"成功讀取 {output_dataset_json_path}, 目前資料筆數: {len(json_obj)}")
+        except:
+            json_obj = []
+            print(f"讀取 {output_dataset_json_path} 失敗")
         print("目前資料筆數:", len(json_obj))
-        with open(output_dataset_json_path, "w") as f:
-            json.dump(json_obj, f)
+
 
     # 5. 訓練模型 (如果資料量太少，可能會失敗，可以考慮用別的資料集) 參考：https://github.com/tloen/alpaca-lora/issues/470
     # TODO: 檢驗模型是否訓練成功
     print("訓練模型")
     llm_adapter_path = f"/home/brick2/platform2024/src/{course_name}/llm_adapter"
     prompt_template_name = "alpaca_tw"
+    prompt_template_name = "alpaca"
     os.system(f"""cd alpaca-lora; 
               
               python {finetune_script_path}\
@@ -71,8 +76,8 @@ def main():
                 --output_dir '{llm_adapter_path}'\
                 --batch_size 32 \
                 --micro_batch_size 16 \
-                --num_epochs 20 \
-                --learning_rate 1e-4 \
+                --num_epochs 3 \
+                --learning_rate 2e-5 \
                 --cutoff_len 512 \
                 --val_set_size 10 \
                 --lora_r 8 \
@@ -85,13 +90,13 @@ def main():
                 ; cd ..
               """)
 
-    # 6. 將 srt, summary, model, QA json, video 的路徑全部都加入資料庫
-    print("將 srt, summary, model, QA json, video 的路徑全部都加入資料庫")
-    os.system(f"""
-              cd ..;
-              python add_to_database.py --course_name {course_name} --playlist_url {playlist_url};
-              cd LLM-automation
-""")
+#     # 6. 將 srt, summary, model, QA json, video 的路徑全部都加入資料庫
+#     print("將 srt, summary, model, QA json, video 的路徑全部都加入資料庫")
+#     os.system(f"""
+#               cd ..;
+#               python add_to_database.py --course_name {course_name} --playlist_url {playlist_url};
+#               cd LLM-automation
+# """)
 
 if __name__ == "__main__":
     main()
